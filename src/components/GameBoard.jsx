8bb017b97c14a39ae2476a667c68b8213f591e84
Kenny Chung <kennychung.techsolution@gmail.com>
2024-04-10 14:09:00 +0800

    fixed game turns

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,19 +5,12 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare, activeSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  function handleSelectSquare(rowIndex, colIndex) {
-    console.log(rowIndex, colIndex);
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activeSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
   }
 
   return (
@@ -27,7 +20,7 @@ export default function GameBoard({ onSelectSquare, activeSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
